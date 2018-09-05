bbbfc4a7c0fb7303ebecf54b5e5c0df315b8f23f
Glitch (screenshot-api) <none>
2018-09-05 03:25:21 +0000

    🎩🍦 Updated with Glitch

--- server.js
+++ server.js
@@ -87,23 +87,25 @@ app.get("/display", (req,res) => {
   var width = req.query.width;
   var imagename = req.query.imagename
   const path = 'public/screenshots/'+imagename+'.png'
-  if (fs.existsSync(path)) {
-    fs.stat(path, function(err, stats){
-      let modified = stats.mtime
-      let seconds = (new Date().getTime() - modified) / 1000;
-      let minutes = seconds/60
-      let hours = minutes/60
-      let days = hours/24
-      console.log(`File modified ${minutes} minutes ago`);
-      if (days < 1){
-        res.json({"image":`https://screenshot-api.glitch.me/screenshots/${imagename}.png`,"created":modified});
-      }else{
-        createShot()
-      }  
-    });
-  }else{
-  createShot()
-  }
+  
+  createShot();
+  // if (fs.existsSync(path)) {
+  //   fs.stat(path, function(err, stats){
+  //     let modified = stats.mtime
+  //     let seconds = (new Date().getTime() - modified) / 1000;
+  //     let minutes = seconds/60
+  //     let hours = minutes/60
+  //     let days = hours/24
+  //     console.log(`File modified ${minutes} minutes ago`);
+  //     if (days < 1){
+  //       res.json({"image":`https://screenshot-api.glitch.me/screenshots/${imagename}.png`,"created":modified});
+  //     }else{
+  //       createShot()
+  //     }  
+  //   });
+  // }else{
+  // createShot()
+  // }
 
 
   
